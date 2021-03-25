67d551378cb6dfe6eaf53ca5f3191bf800ac2870
raymundo_mosqueda <iracoon2014@gmail.com>
2021-03-25 12:31:09 -0600

    Custom Cube

---
+++ Proyecto Algebra Lineal/Assets/Scripts/2do Parcial/MakeCube.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeCube : MonoBehaviour
+{
+	Mesh mesh;
+	MeshRenderer rend;
+	MeshFilter filter;
+	public Material mat;
+	
+	Vector3[] vertex;
+	int [] triangles;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+	    Build();
+    }
+    
+	void Build()
+	{
+		filter = gameObject.AddComponent<MeshFilter>();
+		rend = gameObject.AddComponent<MeshRenderer>();
+		rend.material = mat;		
+		GetComponent<MeshFilter>().mesh = mesh;
+		vertex = new[]
+		{
+			new	Vector3(0,0,0), 
+			new	Vector3(0,1,0),
+			new	Vector3(1,0,0),
+			new Vector3(1,1,0),
+			
+			new Vector3(0,1,1),
+			new Vector3(1,1,1),
+			new Vector3(0,0,1),
+			
+			new Vector3(1,0,1)
+
+				
+		};
+		
+		filter.mesh.vertices = vertex;
+		
+		triangles = new[]
+		{
+			// Front
+			0,1,2,
+			1,3,2,
+			// Top
+			1,4,5,
+			5,3,1,
+			// Left
+			0,4,1,
+			0,6,4,
+			// Right
+			2,3,5,
+			5,7,2,
+			// Bottom
+			2,7,0,
+			7,6,0,
+			// Back
+			7,5,4,
+			7,4,6
+			
+			
+		};
+		
+		filter.mesh.triangles = triangles;
+		
+		
+	}
+
+
+	
+}
---
+++ Proyecto Algebra Lineal/Assets/Scripts/2do Parcial/Translation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translation : MonoBehaviour
+{
+	[SerializeField]private GameObject cube;
+	[SerializeField]private Vector3 toMove;
+	
+	void Update()
+	{
+		CheckInput();
+		
+	}
+	
+	void CheckInput()
+	{		
+		if (Input.GetKeyDown("r"))
+		{
+			cube.transform.position = Translate(cube.transform.position, toMove);
+		}
+		
+	}
+
+	void Move()
+	{
+		cube.transform.position += toMove;
+		
+	}
+	
+	Vector3 Translate(Vector3 pos, Vector3 delta)
+	{
+		float x = (pos.x * 1) + (pos.x * 0) + (pos.x * 0) + (1 * delta.x);
+		float y = (pos.y * 0) + (pos.y * 1) + (pos.y * 0) + (1 * delta.y);
+		float z = (pos.z * 0) + (pos.z * 0) + (pos.z * 1) + (1 * delta.z);
+		
+		return new Vector3(x,y,z);
+	}
+	
+}
